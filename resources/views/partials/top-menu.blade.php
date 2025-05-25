94f018ddf76955bc23ea56f16d790afdcc90c12c
unknown <anupshk39@gmail.com>
2025-05-25 20:19:46 +0545

    Top Menu Update just for Testing

--- resources/views/partials/top-menu.blade.php
+++ resources/views/partials/top-menu.blade.php
@@ -1,6 +1,6 @@
 <nav id="desktop-nav">
     <div class="logo" onclick="location.href='{{ route('home') }}'">
-        Anup Shakya
+        Anup Shakyas
     </div>
     <div>
         {{menu('top-menu','partials.menu-items.top-menu-items-desktop')}}
