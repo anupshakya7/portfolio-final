88c2a909decf3d7da9ee4f659ca0ec5fcad8944a
Anup Shakya <anupshk39@gmail.com>
2025-10-27 22:27:33 +0530

    Update from server

---
+++ .scripts/.scripts/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+# Go to the correct project directory
+cd /home/anupshakyacom/public_html || exit 1
+
+echo "Deployment Started ..."
+
+# Check if composer is installed, install if missing
+if ! command -v composer &> /dev/null
+then
+    echo "Composer not found. Installing..."
+    curl -sS https://getcomposer.org/installer | php
+    COMPOSER_CMD="php composer.phar"
+else
+    echo "Composer is already installed."
+    COMPOSER_CMD="composer"
+fi
+
+# Turn On Maintenance Mode or return true
+# if already is in maintenance mode
+(php artisan down) || true
+
+# Pull the latest version of the app
+git pull origin master
+
+# Install composer dependencies
+$COMPOSER_CMD install --optimize-autoloader --no-dev --no-interaction
+
+# Clearing Cache
+php artisan cache:clear
+php artisan config:clear
+
+# Recreate cache
+php artisan optimize
+
+# Run database migrations
+php artisan migrate --force
+
+# Turn OFF Maintenance mode
+php artisan up
+
+echo "Deployment Finished!!!"
