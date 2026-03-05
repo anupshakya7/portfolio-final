4843d1936cec94995de247bf2c3f7594768ca09e
unknown <anupshk39@gmail.com>
2026-03-06 01:09:58 +0545

    Complete Simple Robot txt Generator Tool

--- resources/views/tools/robots.blade.php
+++ resources/views/tools/robots.blade.php
@@ -34,6 +34,9 @@
                 padding: 10px;
                 border-radius: 10px;
                 margin: 20px 0px;
+                width: 70%;
+                text-align: center;
+                box-shadow: inset rgb(17 17 26 / 21%) 0px 4px 16px, inset rgb(17 17 26 / 0%) 0px 8px 24px, inset rgb(17 17 26 / 0%) 0px 16px 56px;
             }
 
             i {
@@ -62,6 +65,14 @@
                 width: 100%;
             }
 
+            .allow-item {
+                margin-top: 10px;
+            }
+
+            .disallow-item {
+                margin-top: 10px;
+            }
+
             .btn-tool {
                 display: inline;
                 color: #ffffff;
@@ -82,26 +93,31 @@
                 background-color: #2c68a0;
             }
 
+            .add_btn_container {
+                display: block !important;
+                margin-top: 10px;
+            }
+
             #add_useragent_btn {
                 float: right;
             }
 
-            #remove_allow {
-                background-color: #dc2626;
+            .remove {
+                background-color: #dc2626 !important;
             }
 
-            #remove_allow:hover {
-                background-color: #b91c1c;
+            .remove:hover {
+                background-color: #b91c1c !important;
             }
 
             .gpa-converter-wrapper label {
                 margin-top: 7px;
             }
 
-            .field-with-delete{
-                display:flex !important;
-                flex-direction: row !important; 
-                gap:10px;
+            .field-with-delete {
+                display: flex !important;
+                flex-direction: row !important;
+                gap: 10px;
             }
 
             @media (max-width: 935px) {
@@ -125,55 +141,96 @@
             <div class="gpa-converter-wrapper">
                 <h2>Generate Robots.txt</h2>
                 <form>
-                    <div>
+                    {{-- <div>
                         <button class="btn-tool" id="add_useragent_btn"><i class="fa-solid fa-plus"></i>Add User Agent</button>
-                    </div>
+                    </div> --}}
                     <div class="user-agent-container">
                         <div>
                             <label for="">User Agent<span class="required">*</span></label>
                             <select id="user_agent">
                                 <option value="">User Agent</option>
-                                <option value="*">*</option>
-                                <option value="googlebot">Googlebot</option>
+                                <optgroup label="All Crawlers">
+                                    <option value="*">*</option>
+                                </optgroup>
+
+                                <optgroup label="Search Engines">
+                                    <option value="Googlebot">Googlebot</option>
+                                    <option value="Bingbot">Bingbot</option>
+                                    <option value="Slurp">Yahoo Slurp</option>
+                                    <option value="DuckDuckBot">DuckDuckBot</option>
+                                    <option value="Baiduspider">Baiduspider</option>
+                                    <option value="YandexBot">YandexBot</option>
+                                </optgroup>
+
+                                <optgroup label="Social Media">
+                                    <option value="facebookexternalhit">Facebook</option>
+                                    <option value="Twitterbot">Twitter</option>
+                                    <option value="LinkedInBot">LinkedIn</option>
+                                </optgroup>
+
+                                <optgroup label="AI Crawlers">
+                                    <option value="GPTBot">GPTBot</option>
+                                    <option value="ClaudeBot">ClaudeBot</option>
+                                    <option value="CCBot">CCBot</option>
+                                </optgroup>
                             </select>
                         </div>
 
                         <div id="allow-url-container">
                             <div>
                                 <label for="allow">Allow<span class="required">*</span></label>
-                                <div class="field-with-delete">
-                                    <input type="text" id="allow" placeholder="Enter your allow url" />
-                                    <button class="btn-tool" id="remove_allow"><i class="fa-solid fa-trash"></i></button>
+                                <div id="allow-multiple-url-container">
+                                    <div class="allow-item">
+                                        <div class="field-with-delete">
+                                            <input type="text" class="allow-input" placeholder="Enter your allow url" />
+                                            <button class="btn-tool remove"><i class="fa-solid fa-trash"></i></button>
+                                        </div>
+                                    </div>
                                 </div>
-
                                 <span class="error_message" id="allow_url_error"></span>
                             </div>
                         </div>
-                        <div>
+                        <div class="add_btn_container">
                             <button class="btn-tool" id="add_allow_btn"><i class="fa-solid fa-plus"></i>Add Allow
                                 Url</button>
                         </div>
-                        <hr/>
+                        <hr />
 
-                        <div>
+                        <div id="disallow-url-container">
                             <div>
                                 <label for="allow">Disallow<span class="required">*</span></label>
-                                <div class="field-with-delete">
-                                    <input type="text" id="disallow" placeholder="Enter your disallow url" />
-                                    <button class="btn-tool" id="remove_disallow"><i class="fa-solid fa-trash"></i></button>
+                                <div id="disallow-multiple-url-container">
+                                    <div class="disallow-item">
+                                        <div class="field-with-delete">
+                                            <input type="text" class="disallow-input"
+                                                placeholder="Enter your disallow url" />
+                                            <button class="btn-tool remove"><i class="fa-solid fa-trash"></i></button>
+                                        </div>
+                                    </div>
                                 </div>
-                                
                                 <span class="error_message" id="disallow_url_error"></span>
                             </div>
+                        </div>
+                        <div class="add_btn_container">
                             <button class="btn-tool" id="add_disallow_btn"><i class="fa-solid fa-plus"></i>Add Disllow
                                 Url</button>
                         </div>
 
 
+                        <div>
+                            <label for="sitemap">Sitemap URL<span class="required">*</span></label>
+                            <div class="field-with-delete">
+                                <input type="text" id="sitemap" placeholder="Enter your sitemap url" />
+                                {{-- <button class="btn-tool remove"><i class="fa-solid fa-trash"></i></button> --}}
+                            </div>
+
+                            <span class="error_message" id="disallow_url_error"></span>
+                        </div>
                     </div>
 
                     <div class="gpa-button-wrapper">
-                        <button class="btn-tool" type="submit" id="qr_code_btn"><i class="fa-solid fa-robot"></i>Generate
+                        <button class="btn-tool" type="submit" id="robot_generate_btn"><i
+                                class="fa-solid fa-robot"></i>Generate
                             Robot
                             Txt</button>
                     </div>
@@ -205,4 +262,102 @@
     </section>
 @endsection
 @push('script')
+    <script>
+        //Add Allow Url
+        document.getElementById('add_allow_btn').addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const container = document.getElementById('allow-multiple-url-container');
+
+            const html = `
+                <div class="allow-item">
+                    <div class="field-with-delete">
+                        <input type="text" class="allow-input" placeholder="Enter your allow url">
+                        <button type="button" class="btn-tool remove">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+
+            container.insertAdjacentHTML("beforeend", html);
+        });
+
+        //Add Disallow Url
+        document.getElementById("add_disallow_btn").addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const container = document.getElementById("disallow-multiple-url-container");
+
+            const html = `
+                <div class="disallow-item">
+                    <div class="field-with-delete">
+                        <input type="text" class="disallow-input" placeholder="Enter your disallow url">
+                        <button type="button" class="btn-tool remove">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </div>
+                </div>
+            `;
+
+            container.insertAdjacentHTML("beforeend", html);
+        });
+
+        //Remove Field
+        document.addEventListener('click', function(e) {
+            if (e.target.closest('.remove')) {
+                const field = e.target.closest(".allow-item, .disallow-item");
+                if (field) {
+                    field.remove();
+                }
+            }
+        });
+
+        document.getElementById('robot_generate_btn').addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const userAgent = document.getElementById('user_agent').value;
+            const sitemap = document.getElementById('sitemap').value;
+
+            const allowInputs = document.querySelectorAll('.allow-input');
+            const disallowInputs = document.querySelectorAll('.disallow-input');
+
+            let robotsTxt = "";
+
+            if (!userAgent) {
+                alert('Please select a User Agent');
+                return;
+            }
+
+            robotsTxt += "User-agent: " + userAgent + "\n";
+
+            allowInputs.forEach(input => {
+                let value = input.value.trim();
+                if (value !== "") {
+                    if (!value.startsWith("/")) {
+                        value = "/" + value;
+                    }
+                    robotsTxt += "Allow: " + value + "\n";
+                }
+            });
+
+            disallowInputs.forEach(input => {
+                let value = input.value.trim();
+                if (value !== "") {
+                    if (!value.startsWith("/")) {
+                        value = "/" + value;
+                    }
+                    robotsTxt += "Disallow: " + value + "\n";
+                }
+            });
+
+            if (sitemap) {
+                robotsTxt += "\nSitemap: " + sitemap;
+            }
+
+            document.querySelector('.robots-preview pre').textContent = robotsTxt;
+        });
+
+        // document.getElementById('')
+    </script>
 @endpush
